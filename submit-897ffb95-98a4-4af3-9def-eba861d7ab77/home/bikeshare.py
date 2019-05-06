573e95b8e37687d61a07eab6dcc0765360f3b47f
Sylvia-Feller <sylvia_feller@web.de>
2019-05-06 16:30:46 +0200

    Add break statement for the outer while loop

--- submit-897ffb95-98a4-4af3-9def-eba861d7ab77/home/bikeshare.py
+++ submit-897ffb95-98a4-4af3-9def-eba861d7ab77/home/bikeshare.py
@@ -193,6 +193,7 @@ def main():
                         more_data = input('Would you like to see more data? Please enter yes or no: ').lower()
                         if more_data not in ('yes', 'y'):
                             break
+                break
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
